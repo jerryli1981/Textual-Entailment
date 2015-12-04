2de62420d16f1757691a3a1f16b562b164cf3f89
Peng Li <jerryli1981@gmail.com>
2015-12-04 02:02:30 -0600

    add seqLSTM

---
+++ LSTM.lua
@@ -0,0 +1,168 @@
+
+-- single layer lstm
+local LSTM, parent = torch.class('LSTM', 'nn.Module')
+
+function LSTM:__init(config)
+	parent.__init(self)
+
+	self.in_dim = config.in_dim
+	self.mem_dim = config.mem_dim or 150
+
+
+	self.master_cell = self:new_cell()
+
+	self.cells = {}
+
+	self.depth = 0
+
+	local ctable_init, ctable_grad, htable_init, htable_grad
+
+	ctable_init = torch.zeros(self.mem_dim)
+	ctable_grad = torch.zeros(self.mem_dim)
+	htable_init = torch.zeros(self.mem_dim)
+	htable_grad = torch.zeros(self.mem_dim)
+
+	self.initial_values = {ctable_init, htable_init}
+
+	self.gradInput = {
+		torch.zeros(self.in_dim),
+		ctable_grad,
+		htable_grad
+	}
+end
+
+function LSTM:new_cell() 
+	local input = nn.Identity()()
+	local cell_prev = nn.Identity()()
+	local hidden_prev = nn.Identity()()
+
+	local new_gate = function()
+		local in_module = nn.Linear(self.in_dim, self.mem_dim)(input)
+		return nn.CAddTable(){in_module, nn.Linear(self.mem_dim, self.mem_dim)(hidden_prev)}
+	end
+
+	local i_gate = nn.Sigmoid()(new_gate())
+	local f_gate = nn.Sigmoid()(new_gate())
+	local update_gate = nn.Tanh()(new_gate())
+	local o_gate = nn.Sigmoid()(new_gate())
+
+	cell = nn.CAddTable(){
+		nn.CMulTable(){f_gate, cell_prev},
+		nn.CMulTable(){i_gate, update_gate}
+	}
+
+	hidden = nn.CMulTable(){o_gate, nn.Tanh()(cell)}
+
+	local mod = nn.gModule({input, cell_prev, hidden_prev}, {cell, hidden})
+
+	if self.master_cell then
+		share_params(mod, self.master_cell)
+	end
+
+	return mod
+
+end
+
+--inputs: T*in_dim tensor, where T is the number of time steps
+--reverse: if true, read the input from right to left
+--Returns the final hidden state of the LSTM ?
+function LSTM:forward(inputs, reverse)
+	local size = inputs:size(1)
+	for t = 1, size do
+		local input = reverse and inputs[size-t+1] or inputs[t]
+		self.depth = self.depth +1
+		local cell = self.cells[self.depth]
+		if cell == nil then
+			cell = self:new_cell()
+			self.cells[self.depth] = cell
+		end
+
+		local prev_output
+
+		if self.depth > 1 then
+			prev_output = self.cells[self.depth -1].output
+		else
+			prev_output = self.initial_values
+		end 
+
+		local outputs = cell:forward({input, prev_output[1], prev_output[2]})
+		local cell, hidden = unpack(outputs)
+		self.output = hidden
+	end
+
+	return self.output
+end
+
+--grad_outputs: T * mem_dim tersor
+function LSTM:backward(inputs, grad_outputs, reverse)
+	local size = inputs:size(1)
+	if self.depth == 0 then
+		error("No cells to backpropagate through")
+	end
+
+	local input_grads = torch.Tensor(inputs:size())
+
+	for t = size, 1, -1 do
+		local input = reverse and inputs[size -t + 1] or inputs[t]
+		local grad_output = reverse and grad_outputs[size -t + 1] or grad_outputs[t]
+
+		local cell = self.cells[self.depth]
+		local grads = {self.gradInput[2], grad_output}
+
+		local prev_output = (self.depth > 1) and self.cells[self.depth -1].output
+											 or self.initial_values
+
+		self.gradInput = cell:backward({input, prev_output[1], prev_output[2]}, grads)
+
+		if reverse then
+			input_grads[size-t +1] = self.gradInput[1]
+		else
+			input_grads[t] = self.gradInput[1]
+		end
+		self.depth = self.depth-1
+	end
+	self:forget()
+	return input_gards
+end
+
+function LSTM:share(lstm, ...)
+  if self.in_dim ~= lstm.in_dim then error("LSTM input dimension mismatch") end
+  if self.mem_dim ~= lstm.mem_dim then error("LSTM memory dimension mismatch") end
+  if self.gate_output ~= lstm.gate_output then error("LSTM output gating mismatch") end
+  share_params(self.master_cell, lstm.master_cell, ...)
+end
+
+function LSTM:zeroGradParameters()
+  self.master_cell:zeroGradParameters()
+end
+
+function LSTM:parameters()
+  return self.master_cell:parameters()
+end
+
+function LSTM:forget()
+	self.depth = 0
+	for i = 1, #self.gradInput do
+		local gradInput = self.gradInput[i]
+		if type(gradInput) == 'table' then
+			for _, t in pairs(gradInput) do t:zero() end
+		else
+			self.gradInput[i]:zero()
+		end
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LSTMSim.lua
@@ -0,0 +1,160 @@
+local LSTMSim = torch.class('LSTMSim')
+
+function LSTMSim:__init(config)
+  self.mem_dim       = config.mem_dim       or 150
+  self.learning_rate = config.learning_rate or 0.05
+  self.batch_size    = config.batch_size    or 25
+  self.reg           = config.reg           or 1e-4
+  self.sim_nhidden   = config.sim_nhidden   or 50
+
+  -- word embedding
+  self.emb_dim = config.emb_vecs:size(2)
+  self.emb_vecs = config.emb_vecs
+
+  self.num_classes = 3
+
+  -- optimizer configuration
+  self.optim_state = { learningRate = self.learning_rate }
+
+  -- KL divergence optimization objective
+  --self.criterion = nn.DistKLDivCriterion()
+  self.criterion = nn.ClassNLLCriterion()
+
+  -- initialize lstm model
+  local lstm_config = {
+    in_dim = self.emb_dim,
+    mem_dim = self.mem_dim,
+    gate_output = false,
+  }
+  
+
+  self.llstm = LSTM(lstm_config)
+  self.rlstm = LSTM(lstm_config)
+
+  -- similarity model
+  self.sim_module = self:new_sim_module()
+
+  local modules = nn.Parallel()
+    :add(self.llstm)
+    :add(self.sim_module)
+  self.params, self.grad_params = modules:getParameters()
+
+  share_params(self.rlstm, self.llstm)
+end
+
+function LSTMSim:new_sim_module()
+  local vecs_to_input
+  local lvec = nn.Identity()()
+  local rvec = nn.Identity()()
+  local mult_dist = nn.CMulTable(){lvec, rvec}
+  local add_dist = nn.Abs()(nn.CSubTable(){lvec, rvec})
+  local cosine_dist = nn.CosineDistance(){lvec, rvec}
+  local vec_dist_feats = nn.JoinTable(1){mult_dist, add_dist}
+  vecs_to_input = nn.gModule({lvec, rvec}, {vec_dist_feats})
+
+   -- define similarity model architecture
+  local sim_module = nn.Sequential()
+    :add(vecs_to_input)
+    :add(nn.Linear(2 * self.mem_dim, self.sim_nhidden))
+    :add(nn.Sigmoid())    -- does better than tanh
+    :add(nn.Linear(self.sim_nhidden, self.num_classes))
+    :add(nn.LogSoftMax())
+  return sim_module
+end
+
+
+function LSTMSim:train(dataset)
+  self.llstm:training()
+  self.rlstm:training()
+  local indices = torch.randperm(dataset.size)
+  local zeros = torch.zeros(self.mem_dim)
+  for i = 1, dataset.size, self.batch_size do
+    xlua.progress(i, dataset.size)
+    local batch_size = math.min(i + self.batch_size - 1, dataset.size) - i + 1
+
+    local feval = function(x)
+      self.grad_params:zero()
+
+      local loss = 0
+      for j = 1, batch_size do
+        local idx = indices[i + j - 1]
+        local lsent, rsent = dataset.lsents[idx], dataset.rsents[idx]
+        local ent = dataset.labels[idx]
+
+        local linputs = self.emb_vecs:index(1, lsent:long()):double()
+        local rinputs = self.emb_vecs:index(1, rsent:long()):double()
+
+        local inputs = {self.llstm:forward(linputs), self.rlstm:forward(rinputs)}
+
+        local output = self.sim_module:forward(inputs)
+      
+        -- compute loss and backpropagate
+        local example_loss = self.criterion:forward(output, ent)
+        loss = loss + example_loss
+        local sim_grad = self.criterion:backward(output, ent)
+        local rep_grad = self.sim_module:backward(inputs, sim_grad)
+
+        local lgrad = torch.zeros(lsent:nElement(), self.mem_dim)
+        local rgrad = torch.zeros(rsent:nElement(), self.mem_dim)
+        lgrad[lsent:nElement()] = rep_grad[1]
+        rgrad[rsent:nElement()] = rep_grad[2]
+
+        self.llstm:backward(linputs, lgrad)
+        self.rlstm:backward(rinputs, rgrad)
+
+      end
+
+      loss = loss / batch_size
+      self.grad_params:div(batch_size)
+
+      -- regularization
+      loss = loss + 0.5 * self.reg * self.params:norm() ^ 2
+      self.grad_params:add(self.reg, self.params)
+      return loss, self.grad_params
+    end
+
+    optim.adagrad(feval, self.params, self.optim_state)
+  end
+  xlua.progress(dataset.size, dataset.size)
+end
+
+-- Predict the similarity of a sentence pair.
+function LSTMSim:predict(lsent, rsent)
+  self.llstm:evaluate()
+  self.rlstm:evaluate()
+
+  local linputs = self.emb_vecs:index(1, lsent:long()):double()
+  local rinputs = self.emb_vecs:index(1, rsent:long()):double()
+  local inputs = {self.llstm:forward(linputs), self.rlstm:forward(rinputs)}
+
+  local output = self.sim_module:forward(inputs)
+  self.llstm:forget()
+  self.rlstm:forget()
+
+  return output
+end
+
+-- Produce similarity predictions for each sentence pair in the dataset.
+function LSTMSim:predict_dataset(dataset)
+
+  local predictions = torch.Tensor(dataset.size, self.num_classes)
+  for i = 1, dataset.size do
+    xlua.progress(i, dataset.size)
+    local lsent, rsent = dataset.lsents[i], dataset.rsents[i]
+    predictions[i] = self:predict(lsent, rsent)
+  end
+  return predictions
+end
+
+function LSTMSim:print_config()
+  local num_params = self.params:nElement()
+  local num_sim_params = self:new_sim_module():getParameters():nElement()
+  printf('%-25s = %d\n',   'num params', num_params)
+  printf('%-25s = %d\n',   'num compositional params', num_params - num_sim_params)
+  printf('%-25s = %d\n',   'word vector dim', self.emb_dim)
+  printf('%-25s = %d\n',   'LSTM memory dim', self.mem_dim)
+  printf('%-25s = %.2e\n', 'regularization strength', self.reg)
+  printf('%-25s = %d\n',   'minibatch size', self.batch_size)
+  printf('%-25s = %.2e\n', 'learning rate', self.learning_rate)
+  printf('%-25s = %d\n',   'sim module hidden dim', self.sim_nhidden)
+end
